9725a9e5fe84b6de40187b386a815754f766dc71
fenixxx <vickychauhan111@gmail.com>
2017-03-20 14:16:42 +0530

    Revert "ferrari: add vulkan permission"
    This reverts commit 33bd41203b0630f531c8e340cfea93925d333791.

--- product/vulkan.mk
+++
@@ -1,8 +0,0 @@
-# Permissions
- PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.vulkan.level-0.xml:system/etc/permissions/android.hardware.vulkan.level.xml \
-    frameworks/native/data/etc/android.hardware.vulkan.version-1_0_3.xml:system/etc/permissions/android.hardware.vulkan.version.xml
-
-# Set OpenGL ES 3.2
- PRODUCT_PROPERTY_OVERRIDES += \
-    ro.opengles.version=196610
